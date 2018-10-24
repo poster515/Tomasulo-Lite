47cddec18af0286e41e05e4f979d2cb48ba180e1
Joseph <jxp2268@rit.edu>
2018-10-23 21:59:19 -0400

    added shift functions
    Added shift functions. Need to be cautious with undefined outputs for non-ALU functions. May just implement protective code in higher level ALU block.

--- ALU.vhd
+++ ALU.vhd
@@ -9,7 +9,7 @@ entity ALU is
 	 carry_in			: in std_logic; --carry in bit from the Control Unit Status Register
 	 data_in_1 			: in std_logic_vector(15 downto 0); --data from RF data out 1
 	 data_in_2 			: in std_logic_vector(15 downto 0); --data from RF data out 2
-	 reg2_field			: in std_logic_vector(4 downto 0);
+	 value_immediate			: in std_logic_vector(4 downto 0);
 	 
 	 --Control signals
 	 reset_n					: in std_logic; --all registers reset to 0 when this goes low
@@ -23,22 +23,24 @@ entity ALU is
     );
 end ALU;
 
---ADD		Add 						0000	
---ADDI	Add immediate			0001	--immediate value will be direct input from CU
---SUB		Subtract					0010	
---SUBI	Subtract immediate	0011	
---MULT	Multiply					0100	
---DIV		Divide					0101	
---SLA		Shift left arith		0110	--shift value is from immediate value
---SRA		Shift right arith		0111	--shift value is from immediate value
---LD		Load from DM			1000  --compute address if needed
---ST		Store to DM				1001  --compute address if needed
---BNEZ	Branch if not zero	1010
---BNE		Branch if not equal	1011
---JMP		Jump						1100
---(unused)	N/A					1101  --potential floating point operation?
---(unused)	N/A					1110
---(unused)	N/A					1111
+	--Instruction Set Architecture--
+	
+--ADD(I)		Add (immediate) 		0000	
+--SUB(I)		Subtract (immediate)	0001	--immediate value will be direct input from CU
+--MULT(I)	Multiply (immediate)	0010	
+--DIV(I)		Divide (immediate)	0011	
+--LOG			Logical ops				0100	
+--ROT(C)		Rotate (with carry)	0101	
+--SFTL		Shift logical			0110	--shift value is from immediate value
+--SFTA		Shift arithmetic		0111	--shift value is from immediate value
+--LD			Load from DM			1000  --compute address if needed
+--ST			Store to DM				1001  --compute address if needed
+--BNEZ		Branch if not zero	1010
+--BNE			Branch if not equal	1011
+--JMP			Jump						1100
+--(unused)	N/A						1101  --potential floating point operation?
+--(unused)	N/A						1110
+--(unused)	N/A						1111
 
 architecture behavioral of ALU is
 	
@@ -101,12 +103,34 @@ architecture behavioral of ALU is
 	component rotate_c is
 		port
 		(
-			data		: IN STD_LOGIC_VECTOR (16 DOWNTO 0);
-			direction		: IN STD_LOGIC ;
+			data			: IN STD_LOGIC_VECTOR (16 DOWNTO 0);
+			direction	: IN STD_LOGIC ;
 			distance		: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
 			result		: OUT STD_LOGIC_VECTOR (16 DOWNTO 0)
 		);
 	end component rotate_c;
+	
+	component shift_logic is
+		port 
+		(
+			data			: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			direction	: IN STD_LOGIC ;
+			distance		: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+			overflow		: OUT STD_LOGIC ;
+			result		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+		);
+	end component shift_logic;
+	
+	component shift_arith IS
+		port
+		(
+			data			: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			direction	: IN STD_LOGIC ;
+			distance		: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+			overflow		: OUT STD_LOGIC ;
+			result		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+		);
+	end component shift_arith;
 	
 	--Add/Sub signal section
 	signal add_sub_c, add_sub_v 	: std_logic;	
@@ -122,12 +146,21 @@ architecture behavioral of ALU is
 	signal divide_result, divide_remainder	: std_logic_vector(15 downto 0);
 	
 	--Logic unit signals
-	signal logic_result	: std_logic_vector(15 downto 0);
+	signal logic_result				: std_logic_vector(15 downto 0);
 	signal logic_neg, logic_zero	: std_logic;
 	
 	--Rotate unit signals
-	signal rotate_result	: std_logic_vector(15 downto 0);
-	signal rotate_c_result	: std_logic_vector(16 downto 0);
+	signal rotate_result				: std_logic_vector(15 downto 0);
+	signal rotate_c_result			: std_logic_vector(16 downto 0);
+	signal rotate_c_in				: std_logic_vector(16 downto 0);
+	
+	--Shift logical unit signals
+	signal shift_logic_overflow	: std_logic;
+	signal shift_logic_result		: std_logic_vector(15 downto 0);
+	
+	--Shift arithmetic unit signals
+	signal shift_arith_overflow	: std_logic;
+	signal shift_arith_result		: std_logic_vector(15 downto 0);
 	
 	--function prototype
 	function zero_check (temp_result : in std_logic_vector(15 downto 0))
@@ -181,20 +214,40 @@ begin
 	rotate_inst	: rotate
 	port map (
 			data			=> data_in_1,
-			direction	=> ALU_inst_sel(0), --'0'=right, '1'=left
-			distance		=> reg2_field(3 downto 0),
+			direction	=> ALU_inst_sel(0), --'0' = left, '1' = right
+			distance		=> value_immediate(3 downto 0),
 			result		=> rotate_result
 		);
 		
 	rotate_c_inst	: rotate_c
 	port map (
-			data			=> carry_in & data_in_1, -- carry included at MSB
-			direction	=> ALU_inst_sel(0),
-			distance		=> reg2_field,
+			data			=> rotate_c_in, -- carry included at MSB
+			direction	=> ALU_inst_sel(0), --'0' = left, '1' = right
+			distance		=> value_immediate,
 			result		=> rotate_c_result
 		);
 	
+	shift_logic_inst	: shift_logic 
+		port map (
+			data			=> data_in_1,
+			direction	=> ALU_inst_sel(0), --'0' = left, '1' = right
+			distance		=> value_immediate(3 downto 0),
+			overflow		=> shift_logic_overflow,
+			result		=> shift_logic_result
+		);
+	
+	shift_arith_unit	: shift_arith
+		port map (
+			data			=> data_in_1,
+			direction	=> ALU_inst_sel(0), --'0' = left, '1' = right
+			distance		=> value_immediate(3 downto 0),
+			overflow		=> shift_arith_overflow,
+			result		=> shift_arith_result
+		);
+	
 	add_sub_sel <= not(ALU_op(3)) and not(ALU_op(2)) and not(ALU_op(1)) and not(ALU_op(0));
+	
+	rotate_c_in <= carry_in & data_in_1;
 	
 	-- Latching Logic Assignments
 	process(reset_n, clk, ALU_op)
@@ -238,38 +291,21 @@ begin
 			ALU_out_1	<= rotate_c_result(15 downto 0);
 			ALU_status	<= zero_check(rotate_c_result(15 downto 0)) & '0' & rotate_c_result(15) & '0';
 			
+		--SHIFT LOGICAL
 		elsif ALU_op = "0110" then
+			ALU_out_1	<= shift_logic_result;
+			ALU_status	<= zero_check(shift_logic_result) & shift_logic_overflow & shift_logic_result(15) & '0';
 			
-			
+		--SHIFT ARITHMETIC
 		elsif ALU_op = "0111" then
+			ALU_out_1	<= shift_arith_result;
+			ALU_status	<= zero_check(shift_arith_result) & shift_arith_overflow & shift_arith_result(15) & '0';			
 			
-			
-		elsif ALU_op = "1000" then  --LD
-			
-			
-		elsif ALU_op = "1001" then  --ST
-			
-			
-		elsif ALU_op = "1010" then  --BNEZ
-			
-			
-		elsif ALU_op = "1011" then  --BNE
-			
-			
-		elsif ALU_op = "1100" then  --JMP
-			
-			
-		elsif ALU_op = "1101" then
-			
-			
-		elsif ALU_op = "1110" then
-			
-			
-		elsif ALU_op = "1111" then
-		
-		
-		else
-			
+		--LD, ST, BNEZ, BNE, JMP --just recirculate results. no use in making a high Z output here (trying to avoid latched logic)
+--		else
+--			ALU_out_1 	<= "XXXXXXXXXXXXXXXX";
+--			ALU_out_2 	<= "XXXXXXXXXXXXXXXX";
+--			ALU_status 	<= "XXXX";
 			
 		end if; --ALU_op
 	end if; -- clock
