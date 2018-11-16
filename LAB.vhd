8ef50551844ffbdd94f3982f8089539d4729dae5
Joseph <jxp2268@rit.edu>
2018-11-15 21:10:27 -0500

    Significant update to LAB.vhd
    Updates include initializing Look Ahead Buffer (LAB), added Memory Or Aux Buffer (MOAB) for memory addresses, added tag system to uniquely identify instructions in LAB and MOAB.

--- LAB.vhd
+++ LAB.vhd
@@ -15,12 +15,12 @@ entity LAB is
 		EX_op1, EX_op2			: in std_logic_vector(4 downto 0); --source registers for instruction in EX stage
 		MEM_op1, MEM_op2		: in std_logic_vector(4 downto 0); --source registers for instruction in MEM stage (results available)
 		WB_op1, WB_op2			: in std_logic_vector(4 downto 0); --source registers for instruction in WB stage (results available)
-		complete_inst_tag		: in integer;	--input from WB stage, which denotes the tag of the instruction that has been written back, only valid for single clock
-		
+		tag_to_commit			: in integer;	--input from WB stage, which denotes the tag of the instruction that has been written back, only valid for single clock
+
 		
 		PM_data_in		: in 	std_logic_vector(15 downto 0);
-		PM_address		: out std_logic_vector(10 downto 0);
-		PM_clk_en		: out std_logic;
+		PC					: out std_logic_vector(10 downto 0);
+		IW					: out std_logic_vector(15 downto 0)
 	);
 end entity LAB;
 
@@ -51,14 +51,23 @@ architecture arch of LAB is
 	--create array of LAB_entry to create LAB, and initialize to all zeroes
 	signal LAB	: LAB_actual;
 	
-	--signal to enable the system clock on the PM
-	signal PM_clk_en	: std_logic;
+	--input buffer for PM (i.e., program instructions)
+	signal PM_data_reg	: std_logic_vector(15 downto 0);
 	
-	--output of PM (i.e., program instructions)
-	signal PM_data	: std_logic_vector(15 downto 0);
+	--PM output register
+	signal PC_reg		: std_logic_vector(10 downto 0);
 	
 	--signal to denote that LAB is full and we need to stall PM input clock
 	signal LAB_full	: std_logic := '0';
+	
+	--register for IW output
+	signal IW_reg		: std_logic_vector(15 downto 0);
+	
+	--signal to denote that the next IW is actually a memory or auxiliary value, and should go to MOAB
+	signal next_IW_to_MOAB : std_logic := '0';
+	
+	--signal for last LAB spot found
+	signal last_LAB_spot	: integer := 0;
 	
 --	component PM is
 --	port (
@@ -90,9 +99,8 @@ architecture arch of LAB is
 	--function to determine if there are any open LAB spots	
 	--if an open spot exists, take it:	(return [spot to take])
 	--if not, return a stall 			:	(return 5 since there is no fifth spot in LAB)
-	function find_LAB_spot(	 	LAB_in	: in 	LAB_actual; 
-										IW			: in 	std_logic_vector(15 downto 0) 	) 
-		return LAB_actual is
+	function find_LAB_spot(	 	LAB_in	: in 	LAB_actual 		) 
+		return integer is
 								
 		variable i 			: integer 								:= 0;	
 		variable LAB_temp : LAB_actual 							:= LAB_in;
@@ -103,12 +111,12 @@ architecture arch of LAB is
 		for i in 0 to 4 loop
 			if(LAB_temp(i).valid	= '0') then
 			
-				return i;
+				return LAB_temp(i).tag;
 				
 			end if;
 		end loop; --for i
 		
-		return 5;
+		return 5; --come here if there are no spots available
 	end function;
 	
 	--function to write new IW into LAB
@@ -131,57 +139,145 @@ architecture arch of LAB is
 	end function;
 	
 	--function to invalidate tag of instruction that has been completed
-	function commit_IW(	 	LAB_in	: in 	LAB_actual; 
-									tag		: in 	integer 		) 
-		return std_logic is
+	function commit_IW(	 	LAB_in	: in 	LAB_actual   ) 
+		return LAB_actual is
 								
-		variable i 			: integer 								:= 0;	
-		variable LAB_temp : LAB_actual 							:= LAB_in;
-		variable IW_temp	: std_logic_vector(15 downto 0) 	:= IW;
+		variable i 			: integer 		:= 0;	
+		variable LAB_temp : LAB_actual 	:= LAB_in;
+		
+	begin
+		--TODO
+		for i in 0 to 4 loop
+			if tag_to_commit_reg = LAB_temp(i).tag then
+				LAB_temp(i).valid = '0';
+				--TODO: find way to also clear MOAB entry for tag if it exists
+			end if; --if tag_to_commit_reg
+		end loop; --for i
+
+		return LAB_temp;
+	end function;
+	
+	--function to dispatch next instruction from LAB
+	function dispatch_LAB0(	 	LAB_in	: in 	LAB_actual  	) 
+		return LAB_actual is
+								
+		variable LAB_temp : LAB_actual 	:= LAB_in;
 		
 	begin
 		
-		for i in 0 to 4 loop
-			if(LAB_temp(i).valid	= '0') then
-				
-				LAB_temp(i).inst 	<= IW_temp;
-				LAB_temp(i).valid <= '1';
-				
-				return '1';
-				
+		IW_reg <= LAB_temp(0).inst;
+		
+		LAB_temp(0).valid = '0';
+		--if we leave the loop, we haven't found a LAB spot. return 0.
+		return LAB_temp;
+	end function;
+	
+	--function to dispatch next instruction from LAB
+	function shift_LAB(	 	LAB_in	: in 	LAB_actual  	) 
+		return LAB_actual is
+								
+		variable LAB_temp : LAB_actual 	:= LAB_in;
+		variable i, j, k	: integer 		:= 0;
+	begin
+		for i in 0 to 3 loop
+			if (LAB_temp(i).valid = '0') then
+				for j in (i + 1) to 4 loop
+					if (LAB_temp(j).valid = '1') then
+						LAB_temp(i).inst 		<= LAB_temp(j).inst;
+						--SWAP TAGS
+						k							<= LAB_temp(i).tag;
+						LAB_temp(i).tag 		<= LAB_temp(j).tag;
+						LAB_temp(j).tag		<= k;
+						--END SWAP TAGS
+						LAB_temp(i).valid 	<= '1';
+						LAB_temp(j).valid 	<= '0'; --invalidate so next loop can use it
+						exit; --exit if next instruction
+					end if;
+				end loop; --for j
 			end if;
 		end loop; --for i
-		
-		--if we leave the loop, we haven't found a LAB spot. return 0.
-		return '0';
-	end function;
 
+		return LAB_temp;
+	end function;
+	
+	--function to buffer in next IW to MOAB for jumps, etc.
+	function write_to_MOAB(	 	MOAB_in			: in MOAB_actual;
+										last_LAB_spot	: in integer;
+										IW					: in std_logic_vector(15 downto 0)	) 
+		return MOAB_actual is
+								
+		variable MOAB_temp 				: MOAB_actual 							:= MOABB_in;
+		variable last_LAB_spot_temp 	: integer 								:= last_LAB_spot;
+		variable IW_temp 					: std_logic_vector(15 downto 0) 	:= IW;
+	begin
+		for i in 0 to 4 loop
+			if (MOAB_temp(i).valid = '0') then
+				MOAB_temp(i).data 	= IW_temp;
+				MOAB_temp(i).tag 		= last_LAB_spot_temp;
+				MOAB_temp(i).valid 	= '1';
+			end if;
+		end loop; --for i
+
+		return MOAB_temp;
+	end function;
+	
 begin
 
-	PM_clk_en <= reset_n and not(stall_pipeline) and not(LAB_full);
-	
 	process(reset_n, sys_clock)
 		begin
 		
 		if(reset_n = '0') then
 			LAB 			<= init_LAB(LAB);
-			--PM_clk_en 	<= '0';
+			PC_reg 		<= '0';
 			
 		elsif rising_edge(sys_clock) then
-			if PM_clk_en = '1' then --we know that PM address progressed 
-				if(find_LAB_spot(LAB, PM_data_in) < 5) then 
-					--there is a spot in the LAB for it, go load IW into LAB
-					LAB <= load_IW(LAB, i, PM_data_in);
-				else
-					--there is no spot in LAB, stall PM_clk_en
-					LAB_full <= '1';
-				
-				end if; --find_LAB_spot
 		
-			else
-				--PM data is stale and should not be buffered into LAB
+			--first just check whether this is an auxiliary value (e.g., memory address)
+			if next_IW_to_MOAB = '1' then
+				MOAB <= write_to_MOAB(MOAB, last_LAB_spot);
+				next_IW_to_MOAB <= '0';
+			end if;
+			
+			--next, if pipeline isn't stalled, just get dispatch zeroth instruction
+			if stall_pipeline = '0' then 
+				--dispatch first (zeroth) instruction in LAB, if it exists
+				if ( LAB(0).valid = '1' ) then
 				
-		end if;
+					LAB 	<= dispatch_LAB0(LAB);
+					LAB 	<= shift_LAB(LAB);
+					
+				end if;
+			end if;
+		
+			last_LAB_spot <= find_LAB_spot(LAB);
+			
+			if( last_LAB_spot < 5 ) then 
+				--there is a spot in the LAB for it, go load IW into LAB
+				LAB 		<= load_IW(LAB, last_LAB_spot, PM_data_reg);
+				
+				--increment PC to get next IW
+				PC_reg 	<= PC_reg + 1;
+				
+				--now check for whether or not there's another IW coming after this one that needs to go into MOAB
+				if (PM_data_reg(15) and PM_data_reg(1) = '1') then
+					next_IW_to_MOAB <= '1';
+				else
+					next_IW_to_MOAB <= '0';
+				end if; --PM_data_reg
+			else
+				--there is no spot in LAB
+				LAB_full <= '1';
+			
+			end if; --find_LAB_spot
+			
+			--now try to reorganize now that the LAB has been dispatched and/or filled
+			--TODO
+			LAB <= data_haz_check(LAB);
+			
+		end if; --reset_n
 	end process;
-
+		tag_to_commit_reg <= tag_to_commit;
+		PM_data_reg	<= PM_data_in;
+		PC <= PC_reg;
+		IW <= IW_reg;
 end architecture arch;
