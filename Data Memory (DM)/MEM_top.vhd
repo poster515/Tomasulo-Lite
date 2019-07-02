35f6bd02fbe498d9e0e3c1f10acdee31bc88b78a
Joseph <jxp2268@rit.edu>
2019-07-02 09:00:29 -0400

    Minor updates to MEM_top
    Need to connect ROB output from WB stage in CU to MEM_top to complete connection, and then test.

--- Data Memory (DM)/MEM_top.vhd
+++ Data Memory (DM)/MEM_top.vhd
@@ -14,7 +14,6 @@ entity MEM_top is
 		reset_n, sys_clock	: in std_logic;	
 		MEM_in_1, MEM_in_2 	: in std_logic_vector(15 downto 0); --from ALU_top_out_1(2)
 		instruction_word		: in std_logic_vector(15 downto 0);	--so we can store in the st_buff with the other information to check against entry in ROB. 
-		inst_is_specul			: in std_logic;	--denotes whether the incoming instruction is speculative or not
 		ROB_in					: in ROB;			--directly from ROB.
 		
 		--Control 
@@ -70,6 +69,7 @@ architecture behavioral of MEM_top is
 	signal DM_data_in_mux_sel, DM_addr_in_mux_sel, DM_wren_in_mux_sel : std_logic;	--select signals that alternate data going to Data Memory
 	signal store_inst, load_inst											: std_logic;	--denotes whether the incoming instruction word is a load or a store
 	
+	signal inst_is_specul	 : std_logic;
 begin
 
 	DM_data_in : mux_2_new
@@ -130,54 +130,75 @@ begin
 			q			=> data_out
 		);
 		
-	DM_data_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
-	DM_addr_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
-	DM_wren_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
-	MEM_out_top_mux_sel 	<= check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) and (load_inst or store_inst);
-
+	--these two instructions do not require implementation in a process
 	store_inst		<= instruction_word(15) and not(instruction_word(14)) and not(instruction_word(13)) and not(instruction_word(12)) and instruction_word(1);
 	load_inst		<= instruction_word(15) and not(instruction_word(14)) and not(instruction_word(13)) and not(instruction_word(12)) and not(instruction_word(1));
 	
-	--buffer_st_in denotes that the incoming instruction/data must be buffered in st_buff since it is a store and either 1) speculative or 2) existing in st_buff under another address
-	buffer_st_in	<= store_inst and (inst_is_specul or check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)));
+	--need triggered process to determine if incoming instruction_word is speculative
+	process(reset_n, ROB_in, instruction_word)
+	begin
+		if reset_n = '0' then
+			inst_is_specul		<= '0';
+		--now compare the incoming instruction against the ROB to determine if it's speculative still/at all
+		else
+			inst_is_specul		<= check_ROB_for_speculation(ROB_in, instruction_word); 
+		end if;		
+	end process;
 	
-	process(reset_n, sys_clock, MEM_in_1, store_inst, load_inst)
+	
+	process(reset_n, sys_clock, MEM_in_1, store_inst, load_inst, inst_is_specul)
 	begin
 		if reset_n = '0' then
 			MEM_out_top_reg	<= "0000000000000000";
 			st_buff				<= init_st_buff(st_buff);
 			st_buff_data		<= "0000000000000000";
-			st_buff_addr		<= "00000000000"
-		
-		elsif (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) = '1') or ((not(load_inst) and (not(store_inst) or inst_is_specul)) = '1') then
-			--have an address match in st_buff - need to add to st_buff instead
-			--determine if we can execute another, non-speculative store first though
-			if st_buff(0).valid = '1' and st_buff(0).specul = '0' then
-				st_buff_wren 	<= '1';					--wr_en signal for storing in DM from st_buff
-				st_buff_data 	<= st_buff(0).data;	--data for storing back to DM from st_buff
-				st_buff_addr 	<= st_buff(0).addr;	--address for storing back to DM from st_buff
-			else
-				--can neither write to DM from ALU nor write to DM from st_buff - just buffer ALU outputs in st_buff
-				st_buff_wren 	<= '0';
-			end if;
+			st_buff_addr		<= "00000000000";
 			
-		elsif rising_edge(sys_clock) then
-			--if load_inst = '1', check store_buffer for data
-			if load_inst = '1' then
-				--if address not in store_buffer, just read DM
-				if check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) = '1' then
-					st_buff_data	<= fetch_st_buff_data(st_buff, MEM_in_1(10 downto 0));
-				end if;
-			end if;
+			DM_data_in_mux_sel 	<= '0';
+			DM_addr_in_mux_sel 	<= '0'; 
+			DM_wren_in_mux_sel 	<= '0';
+			MEM_out_top_mux_sel  <= '0';
 			
-			--now update st_buff. options: 
-				--1) buffer incoming store (buffer_st_in) 
-				--2) shift st_buff down (DM_wren_in_mux_sel)
-				--3) clear/re-mark st_buff instructions as non-speculative (ROB_in) 
-				
-			st_buff		<= update_st_buff(st_buff, MEM_in_1(10 downto 0), MEM_in_2, buffer_st_in, DM_wren_in_mux_sel, ROB_in, instruction_word); 
+			buffer_st_in	<= '0';
+			
+		else
+			--buffer_st_in denotes that the incoming instruction/data must be buffered in st_buff since it is a store and either 1) speculative or 2) existing in st_buff under another address
+			buffer_st_in			<= store_inst and (inst_is_specul or check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)));
+			
+			DM_data_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
+			DM_addr_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
+			DM_wren_in_mux_sel 	<= (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) or (not(load_inst) and (not(store_inst) or inst_is_specul))) and st_buff(0).valid and not(st_buff(0).specul);
+			MEM_out_top_mux_sel 	<= check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) and (load_inst or store_inst);
 	
-			end if
+			if (check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) = '1') or ((not(load_inst) and (not(store_inst) or inst_is_specul)) = '1') then
+				--have an address match in st_buff - need to add to st_buff instead
+				--determine if we can execute another, non-speculative store first though
+				if st_buff(0).valid = '1' and st_buff(0).specul = '0' then
+					st_buff_wren 	<= '1';					--wr_en signal for storing in DM from st_buff
+					st_buff_data 	<= st_buff(0).data;	--data for storing back to DM from st_buff
+					st_buff_addr 	<= st_buff(0).addr;	--address for storing back to DM from st_buff
+				else
+					--can neither write to DM from ALU nor write to DM from st_buff - just buffer ALU outputs in st_buff
+					st_buff_wren 	<= '0';
+				end if;
+				
+			elsif rising_edge(sys_clock) then
+				--if load_inst = '1', check store_buffer for data
+				if load_inst = '1' then
+					--if address not in store_buffer, just read DM
+					if check_st_buff_for_address(st_buff, MEM_in_1(10 downto 0)) = '1' then
+						st_buff_data	<= fetch_st_buff_data(st_buff, MEM_in_1(10 downto 0));
+					end if;
+				end if;
+				
+				--now update st_buff. options: 
+					--1) buffer incoming store (buffer_st_in) 
+					--2) shift st_buff down (DM_wren_in_mux_sel)
+					--3) clear/re-mark st_buff instructions as non-speculative (ROB_in) 
+					
+				st_buff		<= update_st_buff(st_buff, MEM_in_1(10 downto 0), MEM_in_2, buffer_st_in, DM_wren_in_mux_sel, ROB_in, instruction_word); 
+		
+			end if; 
 		end if; --reset_n
 	end process;
 	
